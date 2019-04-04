e3ea85e7122aba5b8f613a9f49d41bd730c5eef8
Acer <43838095+Hrishi-Raj@users.noreply.github.com>
2019-04-04 23:12:04 +0530

    Automatic update in a sec

--- client.php
+++ client.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="style.css">
+        
     </head>
     <body>
         <div id="main" >
@@ -16,15 +17,21 @@
         </div>
     </body>
     <script >
-        const xhr=new XMLHttpRequest();
+        
+        setInterval(() => {
+            const xhr=new XMLHttpRequest();
+        
+        xhr.open('get','file.txt',true);    
+        xhr.send();
         xhr.onreadystatechange=function() {
             if(xhr.readyState==4)
             {
                 if(xhr.status==200)
                 {
+
                     i=document.getElementById("image");
                     i.src=xhr.responseText;
-                    console.log(xhr.responseText);
+                    console.log("updated");
                 }
                 if(xhr.status==404)
                 {
@@ -33,13 +40,7 @@
             }
         
         };
-        xhr.open('get','file.txt',true);    
-        xhr.send();
-      
+        }, 1000);
     
-    // var c = document.getElementById("image");
-    // c.src="";
-
-
     </script>
 </html>
